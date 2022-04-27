5e2f9f00273a0f452eb9d09ee38389830f28b2c9
Iittlefish <st1030283215852@gmail.com>
2022-04-27 14:10:29 +0800

    feact: create server.ts which can build the server.

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import 'module-alias/register';
+import 'reflect-metadata';
+
+import * as ormConfig from '@/config/config';
+import appInit from './app';
+import { ConnectionOptions } from 'typeorm';
+import {Express} from 'express';
+
+const typeormConfig = ormConfig;
+const port = 3000;
+
+appInit(typeormConfig.default as ConnectionOptions).then(async (app:Express)=>{
+  app.listen(port);
+  console.log(`Example app listening at http://localhost:${port}`);
+}).catch((err) => {
+  console.error(err);
+});
+
